5d228cf223994e6dfb83d6bba67acc8da91de9eb
palkoadam93 <49634997+palkoadam93@users.noreply.github.com>
2024-01-07 17:11:40 +0100

    ok

--- static/shrink.js
+++ static/shrink.js
@@ -1,5 +1,5 @@
 // When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function() {scrollFunction()};
+//window.onscroll = function() {scrollFunction()};
       
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
