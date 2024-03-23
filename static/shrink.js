46ab9911e3ff4acc2e2591586406a8801874e80a
palkoadam93 <49634997+palkoadam93@users.noreply.github.com>
2024-03-23 21:57:33 +0100

    ok

--- static/shrink.js
+++ static/shrink.js
@@ -1,10 +1,10 @@
 // When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
       
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementById("fej").style.padding = "0";
-  } else {
-    document.getElementById("fej").style.padding = "2em 10px";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//     document.getElementById("fej").style.padding = "0";
+//   } else {
+//     document.getElementById("fej").style.padding = "2em 10px";
+//   }
+// }
